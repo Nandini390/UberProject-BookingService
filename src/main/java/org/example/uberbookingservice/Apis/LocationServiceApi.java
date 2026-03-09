a329009bf5ec3184c89e6b7236bbde9e2d979007
Nandini390 <Shara.nandini123@gmail.com>
2026-03-09 20:17:04 +0530

    configured retrofit for Async API comm leveraging service discovery of eureka

---
+++ src/main/java/org/example/uberbookingservice/Apis/LocationServiceApi.java
@@ -0,0 +1,12 @@
+package org.example.uberbookingservice.Apis;
+
+import org.example.uberbookingservice.dto.DriverLocationDto;
+import org.example.uberbookingservice.dto.NearbyDriverDto;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface LocationServiceApi {
+    @POST("/api/location/nearby/drivers")
+    Call<DriverLocationDto[]> getNearbyDrivers(@Body NearbyDriverDto nearbyDriverDto);
+}
--- src/main/java/org/example/uberbookingservice/services/BookingServiceImpl.java
+++ src/main/java/org/example/uberbookingservice/services/BookingServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.uberbookingservice.services;
 
+import org.example.uberbookingservice.Apis.LocationServiceApi;
 import org.example.uberbookingservice.dto.CreateBookingDto;
 import org.example.uberbookingservice.dto.CreateBookingResponseDto;
 import org.example.uberbookingservice.dto.DriverLocationDto;
@@ -12,6 +13,9 @@ import org.example.uberprojectentityservice.Models.Passenger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,14 +27,16 @@ public class BookingServiceImpl implements BookingService{
     private final PassengerRepository passengerRepository;
     private final BookingRepository bookingRepository;
     private final RestTemplate restTemplate;
-    private static final String LOCATION_SERVICE="http://localhost:7777";
+//    private static final String LOCATION_SERVICE="http://localhost:7777";
+    private final LocationServiceApi locationServiceApi;
 
 
     public BookingServiceImpl(PassengerRepository passengerRepository,
-                              BookingRepository bookingRepository, RestTemplate restTemplate){
+                              BookingRepository bookingRepository, RestTemplate restTemplate, LocationServiceApi locationServiceApi){
         this.passengerRepository=passengerRepository;
         this.bookingRepository = bookingRepository;
         this.restTemplate=restTemplate;
+        this.locationServiceApi=locationServiceApi;
     }
 
     @Override
@@ -44,23 +50,52 @@ public class BookingServiceImpl implements BookingService{
                 .build();
         Booking newBooking = bookingRepository.save(booking);
 
-        //make an api call to location service to fetch nearby drivers
+        //make an api call to location service to fetch nearby drivers by RestTemplate
+//        NearbyDriverDto request= NearbyDriverDto.builder()
+//                .latitude(bookingDetails.getStartLocation().getLatitude())
+//                .longitude(bookingDetails.getStartLocation().getLongitude())
+//                .build();
+//        ResponseEntity<DriverLocationDto[]> result = restTemplate.postForEntity(LOCATION_SERVICE+"/api/location/nearby/drivers",request, DriverLocationDto[].class);
+//
+//        if(result.getStatusCode().is2xxSuccessful() && result.getBody()!=null){
+//            List<DriverLocationDto> driverLocations = Arrays.asList(result.getBody());
+//            driverLocations.forEach(driverLocationDto -> {
+//                System.out.println(driverLocationDto.getDriverId() + " " + "lat: " + driverLocationDto.getLatitude()+ " " + "long: " + driverLocationDto.getLongitude());
+//            });
+//        }
+
+        //make an api call to location service to fetch nearby drivers by Retrofit
         NearbyDriverDto request= NearbyDriverDto.builder()
                 .latitude(bookingDetails.getStartLocation().getLatitude())
                 .longitude(bookingDetails.getStartLocation().getLongitude())
                 .build();
-        ResponseEntity<DriverLocationDto[]> result = restTemplate.postForEntity(LOCATION_SERVICE+"/api/location/nearby/drivers",request, DriverLocationDto[].class);
-
-        if(result.getStatusCode().is2xxSuccessful() && result.getBody()!=null){
-            List<DriverLocationDto> driverLocations = Arrays.asList(result.getBody());
-            driverLocations.forEach(driverLocationDto -> {
-                System.out.println(driverLocationDto.getDriverId() + " " + "lat: " + driverLocationDto.getLatitude()+ " " + "long: " + driverLocationDto.getLongitude());
-            });
-        }
+        processNearbyDriverAsync(request);
 
         return CreateBookingResponseDto.builder()
                 .bookingId(newBooking.getId())
                 .bookingStatus(newBooking.getBookingStatus().toString())
                 .build();
+    }
+
+    private void processNearbyDriverAsync(NearbyDriverDto requestDto){
+        Call<DriverLocationDto[]> call= locationServiceApi.getNearbyDrivers(requestDto);
+        call.enqueue(new Callback<DriverLocationDto[]>() {
+            @Override
+            public void onResponse(Call<DriverLocationDto[]> call, Response<DriverLocationDto[]> response) {
+                if(response.isSuccessful() && response.body()!=null){
+                  List<DriverLocationDto> driverLocations = Arrays.asList(response.body());
+                  driverLocations.forEach(driverLocationDto -> {
+                  System.out.println(driverLocationDto.getDriverId() + " " + "lat: " + driverLocationDto.getLatitude()+ " " + "long: " + driverLocationDto.getLongitude());
+                });
+            }else{
+                    System.out.println("Request failed"+response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DriverLocationDto[]> call, Throwable t) {
+                 t.printStackTrace();
+            }
+        });
     }
 }
