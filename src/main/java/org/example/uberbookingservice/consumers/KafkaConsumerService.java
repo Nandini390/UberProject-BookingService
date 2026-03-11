01b70fb7f79d1d91ea9d46962d82363d4aa3dc12
Nandini390 <Shara.nandini123@gmail.com>
2026-03-12 00:55:59 +0530

    added Kafka

---
+++ src/main/java/org/example/uberbookingservice/consumers/KafkaConsumerService.java
@@ -0,0 +1,12 @@
+package org.example.uberbookingservice.consumers;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+    @KafkaListener(topics = "sample-topic")
+    public void listen(String message){
+        System.out.println("kafka msg from topic sample-topic inside booking service: " + message);
+    }
+}
