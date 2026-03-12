83b7d2960d98ec91cad93a07217a6c92d0254d11
Nandini390 <Shara.nandini123@gmail.com>
2026-03-12 21:14:05 +0530

    webhook url concept added

--- src/main/java/org/example/uberbookingservice/controller/BookingController.java
+++ src/main/java/org/example/uberbookingservice/controller/BookingController.java
@@ -5,22 +5,34 @@ import org.example.uberbookingservice.dto.CreateBookingResponseDto;
 import org.example.uberbookingservice.dto.UpdateBookingResponseDto;
 import org.example.uberbookingservice.dto.UpdateBookingRequestDto;
 import org.example.uberbookingservice.services.BookingService;
+import org.example.uberbookingservice.services.WebhookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/booking")
 public class BookingController {
 
     private final BookingService bookingService;
+    private final WebhookService webhookService;
 
-    public BookingController(BookingService bookingService){
+    public BookingController(BookingService bookingService,WebhookService webhookService){
         this.bookingService=bookingService;
+        this.webhookService=webhookService;
     }
 
     @PostMapping
     public ResponseEntity<CreateBookingResponseDto> CreateBooking(@RequestBody CreateBookingDto createBookingDto){
+        Map<String,Object> payload = new HashMap<>();
+        payload.put("passengerId",createBookingDto.getPassengerId());
+        payload.put("Start Location",createBookingDto.getStartLocation());
+        payload.put("End Location",createBookingDto.getEndLocation());
+        String webhookURL = "http://localhost:6700/webhook";
+        webhookService.sendWebhook(webhookURL,payload);
        return new ResponseEntity<>(bookingService.createBooking(createBookingDto), HttpStatus.CREATED);
     }
 
---
+++ src/main/java/org/example/uberbookingservice/services/WebhookService.java
@@ -0,0 +1,19 @@
+package org.example.uberbookingservice.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class WebhookService {
+    private final RestTemplate restTemplate;
+
+    public WebhookService(RestTemplate restTemplate){
+        this.restTemplate=restTemplate;
+    }
+
+    public void sendWebhook(String url, Map<String,Object> payload){
+       restTemplate.postForObject(url,payload, String.class);
+    }
+}
